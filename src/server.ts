79c11fd35cbc5aae747eb06238dc53709f5c68e3
Harsh Sharma <harshsharmahs.081@gmail.com>
2026-05-23 22:03:23 +0530

    feat: refactor server setup and add Prisma configuration
    - Removed old server.js and replaced it with a new server.ts file using TypeScript.
    - Added Prisma configuration file (prisma.config.ts) and schema file (schema.prisma) for database management.
    - Updated package.json to include new dependencies for Prisma, TypeScript, and other utilities.
    - Created a temporary directory with .gitkeep to maintain structure.
    - Added TypeScript configuration (tsconfig.json) for improved type checking and module resolution.

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { type Request, type Response } from 'express';
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+const PORT = Number(process.env.PORT)||3000
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("hello world")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is listening on port: ${PORT}`)
+})
